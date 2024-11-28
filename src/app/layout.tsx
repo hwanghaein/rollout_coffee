b28b3cfaf7602c9a89edeb4c11f391a88ddedc47
황혜인 <hhi9037@naver.com>
2024-11-28 16:19:07 +0900

    [style] 글로벌 및 커스텀 폰트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,7 @@
-// 모든 페이지에 적용되는 글로벌 레이아웃
+// RootLayout.tsx
 
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 // 메인 페이지
 
 export default function Home() {
-  return <div> 메인 페이지</div>;
+  return (
+    <div>
+      <div> 메인 페이지</div>
+    </div>
+  );
 }
