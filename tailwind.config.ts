613bea419ba8e02ca210f1e9dcbd44bee0207879
황혜인 <hhi9037@naver.com>
2024-11-28 22:44:24 +0900

    [chore] tailwind.config.ts 커스텀 스타일 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,20 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: '#2E5021',
+        secondary: '#006633',
+        dark1: '#3F4141',
+        dark2: '#222222',
+        dark3: '#333333',
+        dark4: '#444444',
+        gray1: '#D9D9D9',
+        gray2: '#666666',
+        gray3: '#4E4E4E',
+        brown1: '#352A1C',
+        brown2: '#24211D',
+      },
+      fontSize: {
+        '13': '0.8125rem',  // 13px를 rem으로 변환한 값 (13px ÷ 16px = 0.8125rem)
       },
     },
   },
