4e39394e3e3fb8eebdc5321de705abe9d6b0a1fd
황혜인 <hhi9037@naver.com>
2024-11-28 16:57:13 +0900

    [feat] 글로벌 레이아웃 구성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
+//글로벌 레이아웃
+
 import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "ROLL OUT COFFEE",
-  description: "롤아웃커피는 신선한 커피와 함께 다양한 디저트를 제공합니다. 편안한 분위기에서 특별한 시간을 보낼 수 있는 커피숍입니다.",
+  description:
+    "롤아웃커피는 신선한 커피와 함께 다양한 디저트를 제공합니다. 편안한 분위기에서 특별한 시간을 보낼 수 있는 커피숍입니다.",
   icons: {
     icon: "/favicon.ico",
   },
@@ -15,9 +18,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased">
-        {children}
+    <html lang="ko">
+      <body>
+        <div>
+          {/* <Header/> */}
+          <main>{children}</main>
+          {/* <Footer/> */}
+        </div>
       </body>
     </html>
   );
