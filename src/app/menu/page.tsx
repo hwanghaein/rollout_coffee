6e2ede1beb723c90b289d7f26b500f1f12b02741
황혜인 <hhi9037@naver.com>
2024-11-29 22:21:43 +0900

    [feat] 메뉴 페이지 필터 기능 구현

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,8 +1,40 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
+import { menuItems } from "../../mock/menu";
+import { menuCategories } from "../../mock/menu-category";
+
+const initialFilters = {
+  all: true,
+  signature: false,
+  coffee: false,
+  drip: false,
+  drink: false,
+  sweetTea: false,
+  tea: false,
+  dessert: false,
+};
 
 export default function Page() {
+  // 필터 상태 관리
+  const [filters, setFilters] = useState(initialFilters);
+
+  // 체크박스 상태 변경 함수
+  const handleFilterChange = (category: string) => {
+    setFilters((prevState) => {
+      if (category === "all") {
+        return initialFilters;
+      } else {
+        return {
+          ...prevState,
+          all: false,
+          [category]: !prevState[category],
+        };
+      }
+    });
+  };
+
   return (
     <div className="px-4 md:w-full max-w-[1100px] mx-auto flex flex-col pt-7 pb-20">
       <span className="text-dark2 text-4xl mb-10">MENU</span>
@@ -30,48 +62,43 @@ export default function Page() {
         <div>
           <ul className="grid grid-cols-2 gap-4 md:grid-cols-4 text-sm text-dark1 mt-5">
             {/* 필터 */}
-            <li className="flex items-start items-center gap-2">
-              <input type="checkbox" />
-              <span>필터 1</span>
-            </li>
-            <li className="flex items-start items-center gap-2">
-              <input type="checkbox" />
-              <span>필터 2</span>
-            </li>
-            <li className="flex items-start items-center gap-2">
-              <input type="checkbox" />
-              <span>필터 3</span>
-            </li>
-            {/* 필터 목록 추가 */}
+            {menuCategories.map(({ label, value }) => (
+              <li key={value} className="flex items-start items-center gap-2">
+                <input
+                  type="checkbox"
+                  checked={filters[value]}
+                  onChange={() => handleFilterChange(value)}
+                />
+                <span>{label}</span>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
 
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-5">
         {/* 메뉴 */}
-        <div className="flex flex-col items-center gap-2">
-          <Image
-            src="/images/sample-image.png"
-            alt="샘플 이미지"
-            width={258}
-            height={270}
-            className="object-cover"
-            priority
-          />
-          <span>메뉴 이름</span>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <Image
-            src="/images/sample-image.png"
-            alt="샘플 이미지"
-            width={258}
-            height={270}
-            className="object-cover"
-            priority
-          />
-          <span>메뉴 이름</span>
-        </div>
-        {/* 메뉴 목록 추가 */}
+        {menuItems
+          .filter((item) => {
+            if (filters.all) {
+              return true; 
+            }
+
+            return filters[item.category];
+          })
+          .map((item, index) => (
+            <div key={index} className="flex flex-col items-center gap-2">
+              <Image
+                src={item.src}
+                alt={item.alt}
+                width={258}
+                height={270}
+                className="object-cover"
+                priority
+              />
+              <span>{item.name}</span>
+            </div>
+          ))}
       </div>
     </div>
   );
---
+++ src/mock/menu-category.ts
@@ -0,0 +1,10 @@
+export const menuCategories = [
+  { label: "ALL", value: "all" },
+  { label: "SIGNATURE", value: "signature" },
+  { label: "COFFEE", value: "coffee" },
+  { label: "DRIP", value: "drip" },
+  { label: "DRINK", value: "drink" },
+  { label: "SWEET TEA", value: "sweetTea" },
+  { label: "TEA", value: "tea" },
+  { label: "DESSERT", value: "dessert" },
+];
