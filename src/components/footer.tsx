60e591660d9e3d9a906801fc2a92ea1f2e3573dc
황혜인 <hhi9037@naver.com>
2024-11-29 12:07:12 +0900

    [style] 푸터 스타일링

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,16 @@
 export default function Footer() {
   return (
-    <footer>
-    <div>사업자 등록 번호 102-05-08545</div>
-    <div>롤아웃 커피 대표 박영도</div>
-    <div>TEL: 0507-1392-5605</div>
-    <div>2024 Roll Out Coffee. All Right Reserved.</div>
+    <footer className="w-full bg-dark5 text-gray4 px-6 text-center py-10">
+      <div className="min-w-[375px] max-w-[1100px] mx-auto flex justify-center items-center gap-6 text-xs flex flex-col">
+        <div className="flex gap-6">
+        <p>사업자 등록 번호: 102-05-08545</p>
+        <p>롤아웃 커피: 박영도</p>
+        <p>TEL: 0507-1392-5605</p>
+        </div>
+        <div>
+        <p>&copy; 2024 Roll Out Coffee. All Right Reserved.</p>
+        </div>
+      </div>
     </footer>
   );
 }
