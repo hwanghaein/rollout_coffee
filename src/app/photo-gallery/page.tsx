80f886d75b21427f98aa0aed95d28ff929d05783
황혜인 <hhi9037@naver.com>
2024-11-30 00:56:38 +0900

    [style] 포토 갤러리 페이지 마크업

--- src/app/photo-gallery/page.tsx
+++ src/app/photo-gallery/page.tsx
@@ -1,6 +1,27 @@
-// 포토 갤러리 페이지
-// 강의에서 메인(리스트)페이지와 동일
+"use client";
 
-export default function Page(){
-  return <div>포토 갤러리 페이지</div>
+import Image from "next/image";
+import { photo } from "../../mock/photo";
+
+export default function Page() {
+  return (
+    <div className="px-4 md:w-full max-w-[1100px] mx-auto flex flex-col pt-7 pb-20">
+      <span className="text-dark2 text-4xl mb-10">PHOTO GALLERY</span>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-5">
+        {/* 사진 */}
+        {photo.map((item) => (
+          <div key={item.id} className="flex flex-col items-center gap-2">
+            <Image
+              src={item.imgSrc}
+              alt={item.altText}
+              width={258}
+              height={270}
+              className="object-cover"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
