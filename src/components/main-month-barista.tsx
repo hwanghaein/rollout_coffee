d6e50c9e6b1017173377cfcac8d0373553e7d653
황혜인 <hhi9037@naver.com>
2024-12-03 21:41:48 +0900

    [style] 메인 페이지 이 달의 바리스타 컴포넌트 스타일 수정

--- src/components/main-month-barista.tsx
+++ src/components/main-month-barista.tsx
@@ -19,7 +19,7 @@ export default function MainMonthBarista() {
         {/* 컨텐츠 레이아웃 */}
         <div className="absolute top-0 left-0 w-full h-full flex items-center md:justify-center md:px-10">
           {/* 왼쪽 이미지: 레이디 그레이 */}
-          <div className="z-10 animate-slide-in-left">
+          <div className="z-10 animate-slide-in-left-delay">
             <Image
               src="/images/main/month_barista_img.png"
               alt="레이디 그레이"
@@ -30,7 +30,7 @@ export default function MainMonthBarista() {
             />
           </div>
           {/* 오른쪽 텍스트 */}
-          <div className="flex flex-col lg:ml-10 animate-slide-in-right md:mr-0 mr-5">
+          <div className="flex flex-col lg:ml-10 animate-slide-in-right-delay md:mr-0 mr-5">
             <Image
               src="/images/main/month_barista_text1.png"
               alt="이 달의 바리스타 추천 음료"
@@ -47,6 +47,9 @@ export default function MainMonthBarista() {
               className="object-cover   min-w-[150px]  max-w-[30%]  md:max-w-[70%] lg:max-w-[460px]" // 반응형 적용
               priority
             />
+                 <button className="text-black md:w-24  mt-2 whitespace-nowrap  w-14 py-1 md:py-2  md:mt-7  md:text-sm sm:text-xs text-[8px]  bg-transparent border-[1px] border-solid border-black rounded transition-all duration-300 hover:bg-black hover:text-white">
+                  자세히 보기
+                </button>
           </div>
         </div>
       </section>
