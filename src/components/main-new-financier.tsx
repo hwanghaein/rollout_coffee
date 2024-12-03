166d6745f379c401c7a28c1b00e49a7e9a18c393
황혜인 <hhi9037@naver.com>
2024-12-03 21:48:32 +0900

    [style] 메인 페이지 new-financier 컴포넌트 마크업

---
+++ src/components/main-new-financier.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Image from "next/image";
+
+export default function MainNewFinancier() {
+  return (
+    <div>
+      <section className="relative w-full h-auto ">
+        {/* 배경 이미지 */}
+        <Image
+          src="/images/main/new_financier_bg.png"
+          alt="휘낭시에 배경"
+          width={1920}
+          height={600}
+          className="object-cover"
+          priority
+        />
+        {/* 컨텐츠 레이아웃 */}
+        <div className="absolute top-0 right-0 w-full h-full flex items-center justify-center md:px-10">
+   
+          {/*  텍스트 */}
+          <div className="flex flex-col items-center justify-center animate-slide-in-right-delay mr-2">
+            <Image
+              src="/images/main/new_financier_text.png"
+              alt="New Financier"
+              width={342}
+              height={31}
+              className="object-cover min-w-[100px] max-w-[30%] md:max-w-[50%] lg:max-w-[342px]" 
+              priority
+            />
+
+                        <button className="text-white md:px-4 md:py-2 md:text-sm  px-2 py-1 sm:text-xs text-[8px]  border-[1px] border-solid border-white rounded transition-all duration-300 hover:bg-white hover:text-black mt-6">
+                  자세히 보기
+                </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
