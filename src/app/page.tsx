d9620f110624565a404809bdf20eac79b8449943
황혜인 <hhi9037@naver.com>
2024-12-04 14:52:12 +0900

    [chore] 컴포넌트 파일명 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 "use client"
-import MainMonthBarista from "@/components/main-month-barista";
-import MainSlide from "@/components/main-slide";
-import MainVisual from "@/components/main-visual";
+import Visual from "@/components/main/visual";
+import NewSlide from "@/components/main/new-slide";
+import MonthBarista from "@/components/main/month-barista";
+import DippingFinancier from "@/components/main/dipping-financier";
+import NewFinancier from "@/components/main/new-financier";
+import PhotoSlide from "@/components/main/photo-slide";
+import Store from "@/components/main/store";
 
 export default function Home() {
   return (
     <div className="flex flex-col ">
-      <div> <MainVisual /> </div>
-      <div> <MainSlide /> </div>
-      <div> <MainMonthBarista /> </div>
+      <div> <Visual /></div>
+      <div> <NewSlide /></div>
+      <div> <MonthBarista /></div>
+      <div> <DippingFinancier /></div>
+      <div> <NewFinancier/></div>
+      <div> <PhotoSlide/></div>
+      <div> <Store/></div>
     </div>
   );
 }
