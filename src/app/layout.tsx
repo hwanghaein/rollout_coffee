608a93bc909a8561685655128b61bb3a8d32b82c
황혜인 <hhi9037@naver.com>
2024-12-05 23:10:46 +0900

    [feat] openGraph 속성 추가 및 썸네일 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-//글로벌 레이아웃
-
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/header";
@@ -12,6 +10,29 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
   },
+  openGraph: {
+    title: "ROLL OUT COFFEE",
+    description:
+      "롤아웃커피는 신선한 커피와 함께 다양한 디저트를 제공합니다. 편안한 분위기에서 특별한 시간을 보낼 수 있는 커피숍입니다.", 
+    siteName: "ROLL OUT COFFEE",
+    images: [
+      {
+        url: "/thumbnail.jpg", 
+        width: 1200, 
+        height: 630, 
+        alt: "ROLL OUT COFFEE 썸네일 이미지",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ROLL OUT COFFEE",
+    description:
+      "롤아웃커피는 신선한 커피와 함께 다양한 디저트를 제공합니다. 편안한 분위기에서 특별한 시간을 보낼 수 있는 커피숍입니다.",
+    images: ["/thumbnail.jpg"], 
+  },
 };
 
 export default function RootLayout({
@@ -22,7 +43,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
-        <div className="flex flex-col  ">
+        <div className="flex flex-col">
           <Header />
           <main>{children}</main>
           <Footer />
