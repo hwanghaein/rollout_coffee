70695147931a22d85f0c7ca76271bb2cfe43af0b
황혜인 <hhi9037@naver.com>
2024-12-06 02:23:05 +0900

    [chore] 파이어베이스에서 가져온 이미지 업로드 테스트용 코드

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,32 +1,56 @@
-"use client"
+"use client";
 
-import { doc, getDoc, updateDoc, setDoc } from "firebase/firestore";
-import fireStore from '../../../firebase/firestore';
+import { useState, useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import fireStore from "../../../firebase/firestore";
+import Image from "next/image";
 
 export default function Page() {
-  const onClickCreate = async () => {
-    try {
-      const userDocRef = doc(fireStore, "photos", '8');
-      await setDoc(userDocRef,  
-        {
-          id: "8",
-          alt: "포토 갤러리 이미지8",
-          date: "2024-12-06",
-        },
-    );
-      console.log("데이터가 성공적으로 추가되었습니다.");
-    } catch (error) {
-      console.error("데이터 추가 실패:", error);
-    }
-  };
+  // 데이터 상태 변수
+  const [imageData, setImageData] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Firestore 데이터 가져오기 함수
+    const fetchData = async () => {
+      try {
+        const userDocRef = doc(fireStore, "menuItems", "1");
+        const data = await getDoc(userDocRef);
+
+        if (data.exists()) {
+          console.log(data.data());
+          const docData = data.data();
+          // 이미지 URL 저장
+          if (docData.images && Array.isArray(docData.images)) {
+            setImageData(docData.images[0]);
+          } else {
+            console.log("이미지 데이터가 존재하지 않습니다.");
+          }
+        } else {
+          console.log("문서가 존재하지 않습니다.");
+        }
+      } catch (error) {
+        console.error("데이터 불러오기 실패:", error);
+      }
+    };
+
+    // 함수 호출
+    fetchData();
+  }, []); // 빈 배열을 전달해 컴포넌트 첫 렌더링 시 한 번만 실행
 
   return (
     <div>
-      <button 
-        onClick={onClickCreate} 
-      >
-        setDoc
-      </button>
+      {/* 이미지 렌더링 */}
+      {imageData ? (
+        <Image
+          src={imageData}
+          alt="비행기"
+          width={1100}
+          height={300}
+          className="object-contain mb-12"
+        />
+      ) : (
+        <p>이미지를 불러오는 중입니다...</p>
+      )}
     </div>
   );
 }
