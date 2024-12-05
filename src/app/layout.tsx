f63e76a48020ca3e4cb266aa26c1925a9d2ed60a
황혜인 <hhi9037@naver.com>
2024-12-05 23:30:09 +0900

    [chore] openGraph url 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,8 @@ export const metadata: Metadata = {
     title: "ROLL OUT COFFEE",
     description:
       "롤아웃커피는 신선한 커피와 함께 다양한 디저트를 제공합니다. 편안한 분위기에서 특별한 시간을 보낼 수 있는 커피숍입니다.", 
-    siteName: "ROLL OUT COFFEE",
+      url: "https://rollout-coffee.vercel.app/",
+      siteName: "ROLL OUT COFFEE",
     images: [
       {
         url: "/thumbnail.png", 
