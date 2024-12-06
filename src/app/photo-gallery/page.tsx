c74ef7cf17b65d1870f59a16e5e4fa59ec6c9533
황혜인 <hhi9037@naver.com>
2024-12-06 13:53:11 +0900

    [feat] 포토갤러리 페이지 firebase에서 데이터 가져오기

--- src/app/photo-gallery/page.tsx
+++ src/app/photo-gallery/page.tsx
@@ -1,19 +1,42 @@
 "use client";
 
+import { useState, useEffect } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import fireStore from "../../../firebase/firestore";
 import Image from "next/image";
-import { photo } from "../../mock/photo";
+import { Photo } from "../../types/Photo";
+
+
 
 export default function Page() {
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(() => {
+    async function fetchPhotos() {
+      try {
+        const querySnapshot = await getDocs(collection(fireStore, "photos"));
+        const fetchedPhotos: Photo[] = [];
+        querySnapshot.forEach((doc) => {
+          fetchedPhotos.push({ id: doc.id, ...doc.data() } as Photo);
+        });
+        setPhotos(fetchedPhotos);
+      } catch (error) {
+        console.error("Error fetching photos:", error);
+      }
+    }
+
+    fetchPhotos();
+  }, []);
+
   return (
     <div className="px-4 md:w-full max-w-[1100px] mx-auto flex flex-col pt-7 pb-20">
       <span className="text-dark2 text-3xl mb-10">PHOTO GALLERY</span>
 
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-5">
-        {/* 사진 */}
-        {photo.map((item) => (
+        {photos.map((item) => (
           <div key={item.id} className="flex flex-col items-center gap-2">
             <Image
-              src={item.imgSrc}
+              src={item.image}
               alt={item.alt}
               width={258}
               height={270}
