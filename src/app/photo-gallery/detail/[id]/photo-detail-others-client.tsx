bc2749f77d645af6707baf84eec3041ff9d4531a
황혜인 <hhi9037@naver.com>
2024-12-09 00:12:06 +0900

    [feat] 포토 갤러리 random에서 slice로 변경

--- src/app/photo-gallery/detail/[id]/photo-detail-others-client.tsx
+++ src/app/photo-gallery/detail/[id]/photo-detail-others-client.tsx
@@ -7,12 +7,14 @@ import { Photo } from "../../../../types/photo";
 
 interface PhotoGalleryClientProps {
   photos: Photo[];
+  currentPhotoId: string; 
 }
 
 export default function PhotoDetailOthersClient({
   photos,
+  currentPhotoId,
 }: PhotoGalleryClientProps) {
-  const [randomPhotos, setRandomPhotos] = useState<Photo[]>([]);
+  const [otherPhotos, setOtherPhotos] = useState<Photo[]>([]);
   const router = useRouter();
 
   const handlePhotoClick = (id: string) => {
@@ -20,22 +22,14 @@ export default function PhotoDetailOthersClient({
   };
 
   useEffect(() => {
-    const shuffleArray = (array: any[]) => {
-      const shuffled = [...array];
-      for (let i = shuffled.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; 
-      }
-      return shuffled;
-    };
-
-    setRandomPhotos(shuffleArray(photos).slice(0, 4));
-  }, [photos]);
+    const filteredPhotos = photos.filter((photo) => photo.id !== currentPhotoId);
+    setOtherPhotos(filteredPhotos.slice(0, 6)); 
+  }, [photos, currentPhotoId]);
 
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center pt-4">
       <div className="grid grid-cols-2 gap-2">
-        {randomPhotos.map((item) => (
+        {otherPhotos.map((item) => (
           <div
             key={item.id}
             className="flex flex-col items-center cursor-pointer"
@@ -45,10 +39,10 @@ export default function PhotoDetailOthersClient({
               <Image
                 src={item.image}
                 alt={item.alt}
-                width={150} 
+                width={150}
                 height={150}
                 className="object-cover transform transition-transform duration-500 hover:scale-110"
-                placeholder="blur" 
+                placeholder="blur"
                 blurDataURL="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zY2hlbWFzL3YyIiB3aWR0aD0iMTUwIiBoZWlnaHQ9IjE1MCIgdmlld0JveD0iMCAwIDE1MCAxNTAiPjxwYXRoIGQ9Ik0gMCAwIFogTCAwIDE1MCAwIDE1MCAxNTAiIHN0eWxlPSJmaWxsOm5vbmU7IiBzdHJva2U9IiMyYjNjYmEiIHN0cm9rZS13aWR0aD0iMSIgLz48L3N2Zz4=" // 회색톤 대체 이미지
               />
             </div>
